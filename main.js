b3b9836124f0dce7e16f88b2639f3180cf10330f
blackyselva <selvam17196@gmail.com>
2023-03-19 17:32:45 +0530

    Adding.

---
+++ main.js
@@ -0,0 +1,16 @@
+// document.write("1. Print N number in sequence loop");
+
+document.write("<br>");
+document.write("<br>");
+
+var n=prompt("Enter a sequence N number");
+var i=1;
+while (i<=n){
+	document.write(i);
+	document.write("<br>");
+	i++;
+}
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
